6eeb5be285aa190edf040d3d0fb58bd26a841b3a
user42 <adrian.varnier@gmail.com>
2021-03-07 17:30:37 +0100

    textxure mapping

--- include/renderer.h
+++ include/renderer.h
@@ -6,7 +6,7 @@
 /*   By: avarnier <avarnier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/28 22:49:15 by avarnier          #+#    #+#             */
-/*   Updated: 2021/03/06 22:01:17 by avarnier         ###   ########.fr       */
+/*   Updated: 2021/03/07 14:30:38 by avarnier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,6 @@ int			get_texture_pixel_north(t_texture *texture, double y);
 int			get_texture_pixel_south(t_texture *texture, double y);
 int			get_texture_pixel_east(t_texture *texture, double y);
 int			get_texture_pixel_west(t_texture *texture, double y);
+void		print_image(t_game *game, double dist, int x);
 
 #endif
