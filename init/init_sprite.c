caad38ebf685e320db2605638a118f92ee09ba1d
user42 <adrian.varnier@gmail.com>
2021-03-17 01:52:37 +0100

    check error adjustment

--- init/init_sprite.c
+++ init/init_sprite.c
@@ -6,7 +6,7 @@
 /*   By: avarnier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/09 14:20:29 by avarnier          #+#    #+#             */
-/*   Updated: 2021/03/16 21:36:50 by avarnier         ###   ########.fr       */
+/*   Updated: 2021/03/17 00:07:35 by avarnier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,8 +51,8 @@ static void	get_sprite_position(t_sprite *sprite, t_param *param)
 		{
 			if (param->map[j][i] == '2')
 			{
-				sprite->x[k] = i * TILE_SIZE;
-				sprite->y[k] = j * TILE_SIZE;
+				sprite->x[k] = i * TILE_SIZE + TILE_SIZE / 2;
+				sprite->y[k] = j * TILE_SIZE + TILE_SIZE / 2;
 				k++;
 			}
 			i++;
--- renderer/render_sprite.c
+++ renderer/render_sprite.c
@@ -6,7 +6,7 @@
 /*   By: avarnier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/09 14:51:04 by avarnier          #+#    #+#             */
-/*   Updated: 2021/03/16 22:34:12 by avarnier         ###   ########.fr       */
+/*   Updated: 2021/03/17 01:48:10 by avarnier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,5 +52,50 @@ static void	sort_sprite(t_sprite *sprite, t_player *player)
 
 void	render_sprite(t_game *game)
 {
-	
+	int	i = 0;
+	double dist_to_projection_plane = game->param->width / 2 / tan(degree_to_radian(FOV) / 2);
+	while (game->sprite->x[i] != -1)
+	{
+		double angle = game->player->rotation_angle + atan2(game->sprite->y[i] - game->player->y, game->sprite->x[i] - game->player->x);
+		if (angle > M_PI)
+			angle = angle - 2 * M_PI;
+		if (angle < -M_PI)
+			angle = angle + 2 * M_PI;
+		angle = fabs(angle);
+		if (angle < degree_to_radian(FOV / 2) + 0.5)
+		{
+			double dx = game->sprite->x[i] - game->player->x;
+			double dy = game->sprite->y[i] - game->player->y;
+			double distance = sqrt(dx * dx + dy * dy);
+			angle = atan2(dy, dx) + game->player->rotation_angle;
+			double width = TILE_SIZE / distance * dist_to_projection_plane;
+			double real_width = width;
+			double height = width;
+			double real_height = width;
+			if (width > game->param->width)
+				width = game->param->width - 1;
+			if (height > game->param->height)
+				height = game->param->height - 1;
+			double x = tan(angle) * dist_to_projection_plane;
+			int left = game->param->width / 2 + x - width / 2;
+			int top = game->param->height / 2 - height / 2;
+			int cx = 0;
+			int cy = 0;
+			while (cx < width)
+			{
+				while (cy < height)
+				{
+					if (left + cx > 0 && top + cy > 0 && left + cx < game->param->width && top + cy < game->param->height
+					&& game->wall_distance[left + cx] > distance)
+						pixel_put(game->image, left + cx, top + cy, get_texture_pixel_sprite(game->texture, 
+						(cx + real_width / 2 - width / 2) / real_width,
+						(cy + real_height / 2 - height / 2) / real_height));
+					cy++;
+				}
+				cy = 0;
+				cx++;
+			}
+		}
+	i++;
+	}
 }
