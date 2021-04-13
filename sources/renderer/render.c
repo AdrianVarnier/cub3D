036cb7d348a2456412427e8cdcc42a7a451df846
user42 <adrian.varnier@gmail.com>
2021-04-13 18:35:46 +0200

    norm

--- sources/renderer/render.c
+++ sources/renderer/render.c
@@ -6,7 +6,7 @@
 /*   By: avarnier <avarnier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/28 23:29:43 by avarnier          #+#    #+#             */
-/*   Updated: 2021/03/11 18:56:07 by avarnier         ###   ########.fr       */
+/*   Updated: 2021/04/13 18:27:51 by avarnier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ static void	pixel_put(t_image *image, int x, int y, int color)
 	*(int *)dst = color;
 }
 
-static void	render_ceil(t_game *game, int x, double wall_stripe_height)
+static void	render_ceil_floor(t_game *game, int x, double wall_stripe_height)
 {
 	int	c;
 
@@ -31,12 +31,6 @@ static void	render_ceil(t_game *game, int x, double wall_stripe_height)
 		pixel_put(game->image, x, c, game->param->ceil);
 		c++;
 	}
-}
-
-static void	render_floor(t_game *game, int x, double wall_stripe_height)
-{
-	int	c;
-
 	c = (game->param->height - 1) / 2 + wall_stripe_height / 2;
 	while (c < game->param->height)
 	{
@@ -45,12 +39,13 @@ static void	render_floor(t_game *game, int x, double wall_stripe_height)
 	}
 }
 
-static void	render_wall(t_game *game, int x, double wall_stripe_height, double real_wall_stripe_height)
+static void	render_wall1(t_game *game, int x, double wall_stripe_height,
+double real_wall_stripe_height)
 {
 	int	c;
 
 	c = 0;
-	while (c  <= wall_stripe_height)
+	while (c <= wall_stripe_height)
 	{
 		if (game->texture->orientation == 'N')
 			pixel_put(game->image, x, (game->param->height - 1) / 2
@@ -62,6 +57,18 @@ static void	render_wall(t_game *game, int x, double wall_stripe_height, double r
 			- wall_stripe_height / 2 + c, get_texture_pixel_south(game->texture,
 			(c + real_wall_stripe_height / 2 - wall_stripe_height / 2)
 			/ real_wall_stripe_height));
+		c++;
+	}
+}
+
+static void	render_wall2(t_game *game, int x, double wall_stripe_height,
+double real_wall_stripe_height)
+{
+	int	c;
+
+	c = 0;
+	while (c <= wall_stripe_height)
+	{
 		if (game->texture->orientation == 'E')
 			pixel_put(game->image, x, (game->param->height - 1) / 2
 			- wall_stripe_height / 2 + c, get_texture_pixel_east(game->texture,
@@ -82,12 +89,13 @@ void		render(t_game *game, double distance, int x)
 	double	real_wall_stripe_height;
 	double	projection_plane_distance;
 
-	projection_plane_distance = game->param->width / 2 / tan(degree_to_radian(FOV) / 2);
+	projection_plane_distance = game->param->width / 2
+	/ tan(degree_to_radian(FOV) / 2);
 	wall_stripe_height = TILE_SIZE / distance * projection_plane_distance;
 	real_wall_stripe_height = TILE_SIZE / distance * projection_plane_distance;
 	if (wall_stripe_height >= game->param->height)
 		wall_stripe_height = game->param->height - 1;
-	render_ceil(game, x, wall_stripe_height);	
-	render_wall(game, x, wall_stripe_height, real_wall_stripe_height);
-	render_floor(game, x, wall_stripe_height);
+	render_ceil_floor(game, x, wall_stripe_height);
+	render_wall1(game, x, wall_stripe_height, real_wall_stripe_height);
+	render_wall2(game, x, wall_stripe_height, real_wall_stripe_height);
 }
--- sources/renderer/render_texture.c
+++ sources/renderer/render_texture.c
@@ -6,7 +6,7 @@
 /*   By: avarnier <avarnier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/06 17:02:40 by avarnier          #+#    #+#             */
-/*   Updated: 2021/03/11 19:14:48 by avarnier         ###   ########.fr       */
+/*   Updated: 2021/04/13 18:32:39 by avarnier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,8 @@ int			get_texture_pixel_north(t_texture *texture, double y)
 		x = 0;
 	if (y < 0)
 		y = 0;
-	color = *(int *)(texture->north->data + ((int)y * texture->north->ls + (int)x * (texture->north->bpp / 8)));
+	color = *(int *)(texture->north->data + ((int)y
+	* texture->north->ls + (int)x * (texture->north->bpp / 8)));
 	return (color);
 }
 
@@ -35,12 +36,13 @@ int			get_texture_pixel_south(t_texture *texture, double y)
 	double	x;
 
 	x = fmod(texture->texturex, TILE_SIZE) / TILE_SIZE * texture->south->width;
-	y = y *  texture->south->height;
+	y = y * texture->south->height;
 	if (x < 0)
 		x = 0;
 	if (y < 0)
 		y = 0;
-	color = *(int *)(texture->south->data + ((int)y * texture->south->ls + (int)x * (texture->south->bpp / 8)));
+	color = *(int *)(texture->south->data + ((int)y
+	* texture->south->ls + (int)x * (texture->south->bpp / 8)));
 	return (color);
 }
 
@@ -50,12 +52,13 @@ int			get_texture_pixel_east(t_texture *texture, double y)
 	double	x;
 
 	x = fmod(texture->texturey, TILE_SIZE) / TILE_SIZE * texture->east->width;
-	y = y *  texture->east->height;
+	y = y * texture->east->height;
 	if (x < 0)
 		x = 0;
 	if (y < 0)
 		y = 0;
-	color = *(int *)(texture->east->data + ((int)y * texture->east->ls + (int)x * (texture->east->bpp / 8)));
+	color = *(int *)(texture->east->data + ((int)y
+	* texture->east->ls + (int)x * (texture->east->bpp / 8)));
 	return (color);
 }
 
@@ -65,18 +68,19 @@ int			get_texture_pixel_west(t_texture *texture, double y)
 	double	x;
 
 	x = fmod(texture->texturey, TILE_SIZE) / TILE_SIZE * texture->west->width;
-	y = y *  texture->west->height;
+	y = y * texture->west->height;
 	if (x < 0)
 		x = 0;
 	if (y < 0)
 		y = 0;
-	color = *(int *)(texture->west->data + ((int)y * texture->west->ls + (int)x * (texture->west->bpp / 8)));
+	color = *(int *)(texture->west->data + ((int)y
+	* texture->west->ls + (int)x * (texture->west->bpp / 8)));
 	return (color);
 }
 
 int			get_texture_pixel_sprite(t_texture *texture, double x, double y)
 {
-		int		color;
+	int	color;
 
 	x = x * texture->sprite->width;
 	y = y * texture->sprite->height;
@@ -84,6 +88,7 @@ int			get_texture_pixel_sprite(t_texture *texture, double x, double y)
 		x = 0;
 	if (y < 0)
 		y = 0;
-	color = *(int *)(texture->sprite->data + ((int)y * texture->sprite->ls + (int)x * (texture->sprite->bpp / 8)));
+	color = *(int *)(texture->sprite->data + ((int)y
+	* texture->sprite->ls + (int)x * (texture->sprite->bpp / 8)));
 	return (color);
 }
